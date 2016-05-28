2aa3d46cae8979a64fa9993749654b1799e88fd3
周鹏 <1179258516@qq.com>
2016-05-28 16:36:14 +0800

    2015/5/28练习

---
+++ Basic Level/1007.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+bool isPrime (int n) 
+{
+	bool res = true;
+	for (int j = 2; j <= sqrt((float)n); j++) 
+	{
+		if (n % j == 0) {
+			res = false;
+			break;
+		}
+	}
+	return res;
+}
+int main () 
+{
+	int n;
+	int len = 0, res = 0;
+	cin>>n;
+	for (int i = 2; i < n - 2; i++) 
+	{
+		if ( isPrime(i) && isPrime(i + 2) ) 
+		{
+			res++;
+		}
+	}
+	cout<<res<<endl;
+	return 0;
+}
