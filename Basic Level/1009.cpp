f3b429ae30e6a436a755d59ed1482d7a9d9ae5d5
周鹏 <1179258516@qq.com>
2016-05-29 16:27:32 +0800

    提交1009并标识错误

---
+++ Basic Level/1009.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+int main () 
+{
+	char str[80];
+	gets(str);
+	string arr[80];
+	string temp = "";
+	int index = 0, len = strlen(str);
+	for (int i = 0; i <= len; i++) 
+	{
+		if (str[i] == ' ' || i == len) 
+		{
+			arr[index++] = temp;
+			temp = "";
+		}
+		else 
+		{
+			temp += str[i];
+		}
+	}
+	for (int i = index - 1; i >= 0; i--) 
+	{
+		if (i == index - 1) cout<<arr[i];
+		else cout<<" "<<arr[i];
+	}
+	return 0;
+}
