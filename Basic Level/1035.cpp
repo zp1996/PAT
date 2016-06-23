2b1305a32db6f8857a15d5a3532fdd343256ac32
周鹏 <1179258516@qq.com>
2016-06-23 13:47:48 +0800

    添加1035

--- Basic Level/1035.cpp
+++ Basic Level/1035.cpp
@@ -38,71 +38,69 @@ bool InsertSort (int* arr, int* target, int len)
 	}
 	return flag;
 }
-void MergeArray (int* arr, int* temp, int left, int mid, int right) 
+void Merge (int* arr, int* temp, int left, int mid, int right)
 {
-	int i = left, m = mid, j = mid + 1, n = right, k = 0;
-	while (i <= m && j <= n) 
-	{
-		if (arr[i] <= arr[j]) 
-			temp[k++] = arr[i++];
-		else
-			temp[k++] = arr[j++];
-	}
-	while (i <= m)
-		temp[k++] = arr[i++];
-	while (j <= n)
-		temp[k++] = arr[j++];
-	for (int x = 0; x < k; x++) 
-		arr[left + x] = temp[x];
+    int i = left, m = mid, j = mid, n = right, k = 0;
+    while (i < m && j < n) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+    while (i < m)
+        temp[k++] = arr[i++];
+    while (j < n)
+        temp[k++] = arr[j++];
+    for (int i = 0; i < k; i++)
+        arr[left + i] = temp[i];
 }
-void MSort (int* arr, int left, int right, int* temp) 
+void MergeSort (int* arr, int len, int* arr2)
 {
-	if (left >= right)
-		return;
-	int mid = (left + right) / 2;
-	// sort the left part
-	MSort(arr, left, mid, temp);
-	// sort the right part
-	MSort(arr, mid + 1, right, temp);
-	// merge two part
-	MergeArray(arr, temp, left, mid, right);
-}
-void MergeSort (int* arr, int N) 
-{
-	int* temp = new int[N];
-	for (int len = 1; len < N; len *= 2) 
-	{
-		for (int i = 0; i < N; i += 2 * len) 
+    int* temp = new int[len];
+	bool flag = true;
+    for (int i = 1; i < len; i *= 2) {
+        // i is the length of already sort child list
+        for (int j = 0; j < len; j += 2 * i) {
+
+            if (j + 2 * i <= len)
+                Merge(arr, temp, j, j + i, j + 2 * i);
+            else if (j + 2 * i > len && j + i <= len)
+                Merge(arr, temp, j, j + i, len);
+        }
+		if (!flag) 
 		{
-			if (i + 2 * len <= N)
-				MergeArray(arr, temp, i, i + len, i + 2 * len - 1);
-			else if (i + 2 * len > N && i + len <= N) 
-				MergeArray(arr, temp, i, i + len, N - 1);				
+			for (int i = 0; i < len - 1; i++) 
+			{
+				cout<<arr[i]<<' ';
+			}
+			cout<<arr[len - 1]<<'\n';
+			return;
 		}
-	}
+		if (judge(arr, arr2, len)) 
+		{
+			flag = false;
+			cout<<"Merge Sort\n";
+		}
+    }
 	delete[] temp;
 }
 int main () 
 {
-	//int N, arr1[100], arr2[100];
-	//cin>>N;
-	//for (int i = 0; i < N; i++) 
-	//	cin>>arr1[i];
-	//for (int i = 0; i < N; i++) 
-	//	cin>>arr2[i];
-	//if (!InsertSort(arr1, arr2, N)) 
-	//{
-	//	int target[100];
-	//	for (int i = 0; i < N; i++) 
-	//	{
-	//		target[i] = i;
-	//	}
-	//}
-	int arr[] = {1, 11, 17, 20, 22, 13, 16, 19, 90, 80};
-	MergeSort(arr, 10);
-	for (int i = 0; i <= 9; i++) 
+	int N, arr1[100], arr2[100], temp1[100], temp2[100];
+	cin>>N;
+	for (int i = 0; i < N; i++) 
 	{
-		cout<<arr[i]<<" ";
+		cin>>arr1[i];
+		temp1[i] = arr1[i];
+	}
+	for (int i = 0; i < N; i++)
+	{
+		cin>>arr2[i];
+		temp2[i] = arr2[i];
+	}
+	if (InsertSort(arr1, arr2, N)) 
+	{
+		MergeSort(temp1, N, temp2);
 	}
  	return 0;
 }
