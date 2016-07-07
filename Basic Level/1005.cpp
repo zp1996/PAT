23b8666abd328909508ceabcf6f4671d8057debe
apple <apple@appledeMacBook-Air.local>
2016-07-07 23:33:09 +0800

    修改bug

--- Basic Level/1005.cpp
+++ Basic Level/1005.cpp
@@ -1,66 +1,56 @@
 #include <iostream>
 using namespace std;
-int main () 
+int main ()
 {
-	int arr[100], res[100]; 
-	int k = 0, n;
-	cin>>n;
-	if (n >= 100) throw n;
-	for (int i = 0; i < n; i++)
-	{
-		cin>>arr[i];
-	}
-	for (int i = 0; i < n; i++) 
-	{
-		if (arr[i] != 0) 
-		{
-			int temp = arr[i];
-			while (temp != 1) 
-			{
-				if (temp % 2 == 0) 
-				{
-					temp = temp / 2;
-				}
-				else 
-				{
-					temp = (3 * temp) + 1;
-				}
-				for (int j = 0; j < n; j++) 
-				{
-					if (arr[j] == temp) arr[j] = 0;
-				}
-			}
-		}
-	}
-	for (int i = 0; i < n; i++) 
-	{
-		if (arr[i] != 0) 
-		{
-			res[k++] = arr[i];
-		}
-	}
-	for (int i = 0; i < k - 1; i++) 
-	{
-		for (int j = i + 1; j < k; j++) 
-		{
-			if (res[i] < res[j]) 
-			{
-				int temp = res[i];
-				res[i] = res[j];
-				res[j] = temp;
-			}
-		}
-	}
-	for (int i = 0; i < k; i++) 
-	{
-		if (i == k - 1) 
-		{
-			cout<<res[i];
-		}
-		else 
-		{
-			cout<<res[i]<<" ";
-		}
-	}
-	cout<<endl;
+    int arr[100], res[100];
+    int k = 0, n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            int temp = arr[i];
+            while (temp != 1)
+            {
+                if (temp & 1)
+                    temp = ((3 * temp) + 1) >> 1;
+                else
+                    temp >>= 1;
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[j] == temp)
+                        arr[j] = 0;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+            res[k++] = arr[i];
+    }
+    for (int i = 0; i < k - 1; i++)
+    {
+        bool flag = false;
+        for (int j = k - 1; j > i; j--)
+        {
+            if (res[i] < res[j])
+            {
+                int x = res[i];
+                res[i] = res[j];
+                res[j] = x;
+                flag = true;
+            }
+        }
+        if (!flag)
+            break;
+    }
+    printf("%d", res[0]);
+    for (int i = 1; i < k; i++)
+    {
+        printf(" %d", res[i]);
+    }
+    printf("\n");
 }
--- Basic Level/README.md
+++ Basic Level/README.md
@@ -2,7 +2,6 @@
 基础题目，[题目地址](https://www.patest.cn/contests/pat-b-practise)
 下面为非满分题目，希望大神能够指点一二：
 
- - 1005.cpp  部分正确  -3分
  - 1008.cpp  部分正确  -2分
  - 1014.cpp  部分正确  -2分
  - 1019.cpp  部分正确  -2分
